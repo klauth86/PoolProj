a418d8e365e16e5e4a44b088450069dc328092d1
Artur Khayrullin <kehel@yandex.ru>
2019-10-22 12:44:16 +0300

    Remove unused

--- PoolProjN/Source/PoolProjN/PoolProjN.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
-
-#include "PoolProjN.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PoolProjN, "PoolProjN" );
--- PoolProjZ/Source/PoolProjZ/PoolProjZ.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
-
-#pragma once
-
-#include "PoolProjZ.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PoolProjZ, "PoolProjZ" );
- 
